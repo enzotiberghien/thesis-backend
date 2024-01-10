4467a1e0a54b3d97c0ebe68125f2b17dcb39051c
enzotiberghien <enzotiberghien28@gmail.com>
2024-01-10 15:49:05 +0100

    cors added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
 const connectDB = require("./config/db")
+const cors = require('cors');
+
 
 const PORT = process.env.PORT || 5000
 
@@ -8,6 +10,7 @@ connectDB()
 
 const app = express()
 
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 
